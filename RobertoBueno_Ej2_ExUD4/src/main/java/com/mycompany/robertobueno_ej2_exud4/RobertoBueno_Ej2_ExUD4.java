56ef4142a0e0e17dcda583c9cbb28dfc1fe5b82c
rbuegar <rbuegar927@g.educaand.es>
2024-02-22 11:43:23 +0100

    rbuegar - Adicion ejercicios examen

---
+++ RobertoBueno_Ej2_ExUD4/src/main/java/com/mycompany/robertobueno_ej2_exud4/RobertoBueno_Ej2_ExUD4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.robertobueno_ej2_exud4;
+
+/**
+ *
+ * @author rbuegar927
+ */
+public class RobertoBueno_Ej2_ExUD4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
